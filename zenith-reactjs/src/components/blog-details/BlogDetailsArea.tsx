b3ed9d213a31dc5d4c06f925452c7f38442c170f
Bradley Matjie <bradleysaint45@gmail.com>
2025-06-18 10:00:11 +0200

    blogs

--- zenith-reactjs/src/components/blog-details/BlogDetailsArea.tsx
+++ zenith-reactjs/src/components/blog-details/BlogDetailsArea.tsx
@@ -1,8 +1,22 @@
 import { Link } from "react-router-dom";
-
+import { useLocation } from "react-router-dom";
+import { blogs } from "../../data";
  
 
+
 const BlogDetailsArea = () => {
+	const { search } = useLocation();
+	const queryParams = new URLSearchParams(search);
+	const id = queryParams.get("id");
+	const title = queryParams.get("title");
+
+	const blog = blogs.find(item => item.id == id);
+
+	
+	console.log("id:", id);
+	console.log("title:",title);
+	console.log(blog);
+
 	return (
 		<>
 			<div className="section-space-y">
@@ -10,7 +24,7 @@ const BlogDetailsArea = () => {
 					<div className="row g-4">
 						<div className="col-lg-8">
 							<img
-								src="assets/img/blog-img-10.png"
+								src={blog?.blogImage}
 								alt="image"
 								className="img-fluid"
 							/>
@@ -19,7 +33,7 @@ const BlogDetailsArea = () => {
 									<span className="d-inline-block flex-shrink-0">
 										<i className="bi bi-calendar2-minus"></i>
 									</span>
-									<p className="mb-0 flex-shrink-0 fs-14">23, Oct 2023</p>
+									<p className="mb-0 flex-shrink-0 fs-14">{blog?.date}</p>
 								</div>
 								<div className="d-flex row-gap-2 column-gap-3 align-items-center">
 									<span className="d-inline-block flex-shrink-0">
@@ -27,73 +41,33 @@ const BlogDetailsArea = () => {
 									</span>
 									<p className="mb-0 flex-shrink-0 fs-14">By Admin</p>
 								</div>
-								<div className="d-flex row-gap-2 column-gap-3 align-items-center">
+								{/* <div className="d-flex row-gap-2 column-gap-3 align-items-center">
 									<span className="d-inline-block flex-shrink-0">
 										<i className="bi bi-chat-square-dots"></i>
 									</span>
 									<p className="mb-0 flex-shrink-0 fs-14">Comment</p>
-								</div>
+								</div> */}
 							</div>
 							<h3 className="d-inline-block text-light">
-								Design System Smashing Design Patterns Are Better Way To
-								Collaborate On Your Design System
+								{blog?.title}
 							</h3>
 							<p className="mb-8">
-								At vero eos et accusamus et iusto odio dignissimos ducimus qui
-								blanditiis praesentium voluptatum deleniti atque corrupti quos
-								dolores et quas molestias excepturi sint fuga. Et harum quidem
-								rerum facilis est et expedita distinctio.
+								{blog?.content}
 							</p>
-							<div className="d-flex gap-4 bg-primary-gradient rounded-4 p-4 p-md-6 p-xl-8 my-8">
-								<div className="d-inline-block flex-shrink-0">
-									<img
-										src="assets/img/icon-quote-light.png"
-										alt="image"
-										className="img-fluid"
-									/>
-								</div>
-								<div className="d-inline-block flex-grow-1">
-									<span className="d-block text-light mb-3">
-										‘’ Embed a little help from our friends from time to time.
-										Although we offer the one-stop convenience of annery
-										integrated range of legal.’’{" "}
-									</span>
-									<span className="d-flex align-items-center gap-2">
-										<span className="d-inline-block w-10 h-2px bg-light flex-shrink-0"></span>{" "}
-										<span className="d-inline-block text-light">
-											Peter Parker
-										</span>
-									</span>
-								</div>
-							</div>
-							<p className="mb-8">
-								At vero eos et accusamus et iusto odio dignissimos ducimus qui
-								blanditiis praesentium voluptatum deleniti atque corrupti quos
-								dolores et quas molestias excepturi sint fuga. Et harum quidem
-								rerum facilis est et expedita distinctio.
-							</p>
+							
+							
 							<div className="section-space-sm-y border-top border-bottom">
 								<div className="row g-4 align-items-center">
 									<div className="col-md-6">
 										<ul className="list list-row align-items-center flex-wrap gap-3">
 											<li>
 												<span className="d-inline-block fw-bold text-light">
-													Tags:
+													Category:
 												</span>
 											</li>
 											<li>
 												<Link to="/blog" className="btn btn-light btn-sm">
-													Business
-												</Link>
-											</li>
-											<li>
-												<Link to="/blog" className="btn btn-light btn-sm">
-													Videos
-												</Link>
-											</li>
-											<li>
-												<Link to="/blog" className="btn btn-light btn-sm">
-													Image
+													{blog?.category}
 												</Link>
 											</li>
 										</ul>
@@ -133,7 +107,7 @@ const BlogDetailsArea = () => {
 									</div>
 								</div>
 							</div>
-							<div className="section-space-sm-y border-bottom">
+							{/* <div className="section-space-sm-y border-bottom">
 								<h4 className="mb-8 text-light">Comments</h4>
 								<ul className="list list-flush list-review">
 									<li>
@@ -290,106 +264,22 @@ const BlogDetailsArea = () => {
 										</div>
 									</div>
 								</div>
-							</div>
+							</div> */}
 						</div>
 						<div className="col-lg-4">
 							<div className="row g-4">
 								<div className="col-12">
 									<div className="p-4 p-md-6 p-xxl-8 bg-dark-gradient rounded-4">
-										<h3 className="mb-0 text-gradient-primary">
-											Service Lists
-										</h3>
-										<hr className="my-5" />
-										<ul className="list gap-4">
-											<li>
-												<Link
-													to="/blog"
-													className="link d-flex justify-content-between align-items-center gap-3 text-light hover:text-primary group"
-												>
-													<span className="d-block flex-grow-1">
-														Article Generation{" "}
-													</span>
-													<span className="d-grid place-content-center w-6 h-6 rounded-circle bg-light text-dark group-hover:bg-primary transition">
-														<i className="bi bi-chevron-right"></i>
-													</span>
-												</Link>
-											</li>
-											<li>
-												<Link
-													to="/blog"
-													className="link d-flex justify-content-between align-items-center gap-3 text-light hover:text-primary group"
-												>
-													<span className="d-block flex-grow-1">
-														Ecommerce Copy{" "}
-													</span>
-													<span className="d-grid place-content-center w-6 h-6 rounded-circle bg-light text-dark group-hover:bg-primary transition">
-														<i className="bi bi-chevron-right"></i>
-													</span>
-												</Link>
-											</li>
-											<li>
-												<Link
-													to="/blog"
-													className="link d-flex justify-content-between align-items-center gap-3 text-light hover:text-primary group"
-												>
-													<span className="d-block flex-grow-1">
-														Sales Copy{" "}
-													</span>
-													<span className="d-grid place-content-center w-6 h-6 rounded-circle bg-light text-dark group-hover:bg-primary transition">
-														<i className="bi bi-chevron-right"></i>
-													</span>
-												</Link>
-											</li>
-											<li>
-												<Link
-													to="/blog"
-													className="link d-flex justify-content-between align-items-center gap-3 text-light hover:text-primary group"
-												>
-													<span className="d-block flex-grow-1">
-														Social media Content{" "}
-													</span>
-													<span className="d-grid place-content-center w-6 h-6 rounded-circle bg-light text-dark group-hover:bg-primary transition">
-														<i className="bi bi-chevron-right"></i>
-													</span>
-												</Link>
-											</li>
-											<li>
-												<Link
-													to="/blog"
-													className="link d-flex justify-content-between align-items-center gap-3 text-light hover:text-primary group"
-												>
-													<span className="d-block flex-grow-1">Ad Copy </span>
-													<span className="d-grid place-content-center w-6 h-6 rounded-circle bg-light text-dark group-hover:bg-primary transition">
-														<i className="bi bi-chevron-right"></i>
-													</span>
-												</Link>
-											</li>
-											<li>
-												<Link
-													to="/blog"
-													className="link d-flex justify-content-between align-items-center gap-3 text-light hover:text-primary group"
-												>
-													<span className="d-block flex-grow-1">
-														Startup tools{" "}
-													</span>
-													<span className="d-grid place-content-center w-6 h-6 rounded-circle bg-light text-dark group-hover:bg-primary transition">
-														<i className="bi bi-chevron-right"></i>
-													</span>
-												</Link>
-											</li>
-										</ul>
-									</div>
-								</div>
-								<div className="col-12">
-									<div className="p-4 p-md-6 p-xxl-8 bg-dark-gradient rounded-4">
 										<h3 className="mb-0 text-gradient-primary">Recent Post</h3>
 										<hr className="my-5" />
 										<ul className="list gap-4">
-											<li>
+											
+											{blogs.slice(0,4).map(item => (
+												<li>
 												<div className="d-flex gap-4 align-items-center">
 													<div className="d-grid place-content-center w-12 h-12 flex-shrink-0">
 														<img
-															src="assets/img/recent-img-1.png"
+															src={item.blogImage}
 															alt="image"
 															className="img-fluid"
 														/>
@@ -400,110 +290,19 @@ const BlogDetailsArea = () => {
 																<i className="bi bi-calendar2-check"></i>{" "}
 															</span>
 															<span className="d-inline-block fs-12">
-																23 Oct, 2023
+																{item.date}
 															</span>
 														</div>
 														<Link
-															to="/blog"
+															to={`/blog-details?id=${item?.id}&title=${encodeURIComponent(item.title)}`}
 															className="link d-inline-block fs-14 fw-medium text-light text-opacity-70 hover:text-opacity-100"
 														>
-															Popular Search finance Manager Customer
+															{item.title}
 														</Link>
 													</div>
 												</div>
 											</li>
-											<li>
-												<div className="d-flex gap-4 align-items-center">
-													<div className="d-grid place-content-center w-12 h-12 flex-shrink-0">
-														<img
-															src="assets/img/recent-img-2.png"
-															alt="image"
-															className="img-fluid"
-														/>
-													</div>
-													<div className="flex-grow-1">
-														<div className="d-flex align-items-center gap-2 mb-1">
-															<span className="d-inline-block fs-12">
-																<i className="bi bi-calendar2-check"></i>{" "}
-															</span>
-															<span className="d-inline-block fs-12">
-																23 Oct, 2023
-															</span>
-														</div>
-														<Link
-															to="/blog"
-															className="link d-inline-block fs-14 fw-medium text-light text-opacity-70 hover:text-opacity-100"
-														>
-															Leonardo Da Vinci Can Teach Us About Web
-														</Link>
-													</div>
-												</div>
-											</li>
-											<li>
-												<div className="d-flex gap-4 align-items-center">
-													<div className="d-grid place-content-center w-12 h-12 flex-shrink-0">
-														<img
-															src="assets/img/recent-img-3.png"
-															alt="image"
-															className="img-fluid"
-														/>
-													</div>
-													<div className="flex-grow-1">
-														<div className="d-flex align-items-center gap-2 mb-1">
-															<span className="d-inline-block fs-12">
-																<i className="bi bi-calendar2-check"></i>{" "}
-															</span>
-															<span className="d-inline-block fs-12">
-																23 Oct, 2023
-															</span>
-														</div>
-														<Link
-															to="/blog"
-															className="link d-inline-block fs-14 fw-medium text-light text-opacity-70 hover:text-opacity-100"
-														>
-															Dynamic Donut Charts WIth Tailwlnd Recat
-														</Link>
-													</div>
-												</div>
-											</li>
-										</ul>
-									</div>
-								</div>
-								<div className="col-12">
-									<div className="p-4 p-md-6 p-xxl-8 bg-dark-gradient rounded-4">
-										<h3 className="mb-0 text-gradient-primary">Tags:</h3>
-										<hr className="my-5" />
-										<ul className="list list-row flex-wrap gap-3">
-											<li>
-												<Link to="/blog" className="btn btn-light btn-sm">
-													Business
-												</Link>
-											</li>
-											<li>
-												<Link to="/blog" className="btn btn-light btn-sm">
-													Videos
-												</Link>
-											</li>
-											<li>
-												<Link to="/blog" className="btn btn-light btn-sm">
-													Image
-												</Link>
-											</li>
-											<li>
-												<Link to="/blog" className="btn btn-light btn-sm">
-													Content
-												</Link>
-											</li>
-											<li>
-												<Link to="/blog" className="btn btn-light btn-sm">
-													Media Post
-												</Link>
-											</li>
-											<li>
-												<Link to="/blog" className="btn btn-light btn-sm">
-													Design
-												</Link>
-											</li>
+											))}
 										</ul>
 									</div>
 								</div>
